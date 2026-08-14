eee979eefb0d3ced951040f6133e6ebaa0c08910
pargev88-dev <pargev88@gmail.com>
2026-08-13 21:44:01 -0400

    Add GA4 tag (G-ZWXLS2X3YS) via external config to satisfy CSP

---
+++ public/js/analytics.js
@@ -0,0 +1,7 @@
+// Loaded as an external file because the site CSP has no 'unsafe-inline' for scripts.
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag('js', new Date());
+gtag('config', 'G-ZWXLS2X3YS');
